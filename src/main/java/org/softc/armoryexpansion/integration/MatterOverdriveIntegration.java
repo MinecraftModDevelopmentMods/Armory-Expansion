19bdbf51c00ed60e60505e12d4607f6d600b2747
YaibaToKen <kurokyoudai@live.com>
2019-05-13 16:26:47 +0100

    Added basic Matter Overdrive Integration

---
+++ src/main/java/org/softc/armoryexpansion/integration/MatterOverdriveIntegration.java
@@ -0,0 +1,38 @@
+package org.softc.armoryexpansion.integration;
+
+import com.github.alexthe666.iceandfire.IceAndFire;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.softc.armoryexpansion.ArmoryExpansion;
+import org.softc.armoryexpansion.integration.aelib.JsonIntegration;
+
+@Mod(
+        modid = IceAndFireIntegration.MODID,
+        name = IceAndFireIntegration.NAME,
+        version = ArmoryExpansion.VERSION,
+        dependencies = IceAndFireIntegration.DEPENDENCIES
+)
+@Mod.EventBusSubscriber
+public class MatterOverdriveIntegration extends JsonIntegration {
+    static final String MODID = ArmoryExpansion.MODID + "-matteroverdrive";
+    static final String NAME = ArmoryExpansion.NAME + " - MatterOverdrive";
+    static final String DEPENDENCIES =
+            "required-after:" + ArmoryExpansion.MODID + "; " +
+                    "after:matteroverdrive; ";
+
+    public MatterOverdriveIntegration() {
+        super("matteroverdrive", "assets/" + ArmoryExpansion.MODID + "/data/matteroverdrive.json");
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        this.modid = IceAndFire.MODID;
+        super.preInit(event);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        super.init(event);
+    }
+}
